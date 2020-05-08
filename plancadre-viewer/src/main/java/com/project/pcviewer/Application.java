c7bb080da85fc6a9ab5517f784a2b1abdc720640
Felix Pelletier <felixbig@hotmail.fr>
2020-05-08 12:10:09 -0400

    Fixed utf-8

--- plancadre-viewer/src/main/java/com/project/pcviewer/Application.java
+++ plancadre-viewer/src/main/java/com/project/pcviewer/Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+		System.setProperty("file.encoding","UTF-8");
+		Field charset = Charset.class.getDeclaredField("defaultCharset");
+		charset.setAccessible(true);
+		charset.set(null,null);
 		SpringApplication.run(Application.class, args);
 	}
 
